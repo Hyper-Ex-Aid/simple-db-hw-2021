389138d4581f8a49539b7990b84c46c86200a28e
Hyper-Ex-Aid <smokykk@163.com>
2024-11-25 21:33:58 +0800

    HeapPageId通过测试

--- src/java/simpledb/storage/HeapPageId.java
+++ src/java/simpledb/storage/HeapPageId.java
@@ -3,60 +3,72 @@ package simpledb.storage;
 /** Unique identifier for HeapPage objects. */
 public class HeapPageId implements PageId {
 
+    private int tableId;
+    private int pgNo;
+
     /**
-     * Constructor. Create a page id structure for a specific page of a
-     * specific table.
+     * Constructor. Create a page id structure for a specific page of a specific table.
      *
-     * @param tableId The table that is being referenced
-     * @param pgNo The page number in that table.
+     * @param tableId
+     *         The table that is being referenced
+     * @param pgNo
+     *         The page number in that table.
      */
     public HeapPageId(int tableId, int pgNo) {
         // some code goes here
+        this.tableId = tableId;
+        this.pgNo = pgNo;
     }
 
     /** @return the table associated with this PageId */
     public int getTableId() {
         // some code goes here
-        return 0;
+        return tableId;
     }
 
     /**
-     * @return the page number in the table getTableId() associated with
-     *   this PageId
+     * @return the page number in the table getTableId() associated with this PageId
      */
     public int getPageNumber() {
         // some code goes here
-        return 0;
+        return pgNo;
     }
 
     /**
-     * @return a hash code for this page, represented by a combination of
-     *   the table number and the page number (needed if a PageId is used as a
-     *   key in a hash table in the BufferPool, for example.)
+     * @return a hash code for this page, represented by a combination of the table number and the page number (needed if a PageId is used
+     *         as a key in a hash table in the BufferPool, for example.)
      * @see BufferPool
      */
     public int hashCode() {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
+        try{
+            return (String.valueOf(tableId)+String.valueOf(pgNo)).hashCode();
+        }catch (UnsupportedOperationException exception){
+            throw exception;
+        }
     }
 
     /**
      * Compares one PageId to another.
      *
-     * @param o The object to compare against (must be a PageId)
-     * @return true if the objects are equal (e.g., page numbers and table
-     *   ids are the same)
+     * @param o
+     *         The object to compare against (must be a PageId)
+     * @return true if the objects are equal (e.g., page numbers and table ids are the same)
      */
     public boolean equals(Object o) {
         // some code goes here
+        if(this.getClass().isInstance(o)){
+            PageId pageId = (PageId) o;
+            if(tableId==pageId.getTableId()&&pgNo==pageId.getPageNumber()){
+                return true;
+            }
+        }
         return false;
     }
 
     /**
-     *  Return a representation of this object as an array of
-     *  integers, for writing to disk.  Size of returned array must contain
-     *  number of integers that corresponds to number of args to one of the
-     *  constructors.
+     * Return a representation of this object as an array of integers, for writing to disk.  Size of returned array must contain number of
+     * integers that corresponds to number of args to one of the constructors.
      */
     public int[] serialize() {
         int[] data = new int[2];
