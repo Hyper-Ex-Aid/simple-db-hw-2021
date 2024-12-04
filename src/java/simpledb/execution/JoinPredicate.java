deb88f143f81dbc42b990ed0f4d8f41ab0d409d9
Hyper-Ex-Aid <smokykk@163.com>
2024-12-04 19:43:18 +0800

    JoinPredicate通过测试

--- src/java/simpledb/execution/JoinPredicate.java
+++ src/java/simpledb/execution/JoinPredicate.java
@@ -8,13 +8,19 @@ import java.io.Serializable;
 /**
  * JoinPredicate compares fields of two tuples using a predicate. JoinPredicate
  * is most likely used by the Join operator.
+ * JoinPredicate使用谓词比较两个元组的字段，JoinPredicate最有可能由Join运算符使用
  */
 public class JoinPredicate implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private final int field1;
+    private final int field2;
+    private final Predicate.Op op;
+
     /**
      * Constructor -- create a new predicate over two fields of two tuples.
+     * 在两个元组的两个字段上创建一个新的谓词
      * 
      * @param field1
      *            The field index into the first tuple in the predicate
@@ -29,34 +35,39 @@ public class JoinPredicate implements Serializable {
      */
     public JoinPredicate(int field1, Predicate.Op op, int field2) {
         // some code goes here
+        this.field1=field1;
+        this.field2=field2;
+        this.op=op;
+
     }
 
     /**
      * Apply the predicate to the two specified tuples. The comparison can be
      * made through Field's compare method.
+     * 将谓词应用于两个指定的元组，可以通过field的比较方法进行比较
      * 
      * @return true if the tuples satisfy the predicate.
      */
     public boolean filter(Tuple t1, Tuple t2) {
         // some code goes here
-        return false;
+        return t1.getField(field1).compare(op,t2.getField(field2));
     }
     
     public int getField1()
     {
         // some code goes here
-        return -1;
+        return field1;
     }
     
     public int getField2()
     {
         // some code goes here
-        return -1;
+        return field2;
     }
     
     public Predicate.Op getOperator()
     {
         // some code goes here
-        return null;
+        return op;
     }
 }
